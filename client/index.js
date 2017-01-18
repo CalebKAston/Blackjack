3f55d9ac50a7c9b0911b8069631630957e85f9b6
Caleb Aston <caleb.k.aston@gmail.com>
2017-01-18 11:19:33 -0700

    Applied a bootstrap theme to template the page in the desired manner.

--- client/index.js
+++ client/index.js
@@ -2,4 +2,6 @@ var blackjack = angular.module("blackjack", []);
 
 blackjack.controller("blackjackController", function($scope){
   $scope.helloWorld = "Hello World!!!!";
+  $scope.playerScore = 0;
+  $scope.dealerScore = 0;
 });
