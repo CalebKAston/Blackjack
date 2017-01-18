9706352db5ce6f227a9470880c4191aced428b31
Caleb Aston <caleb.k.aston@gmail.com>
2017-01-18 16:30:18 -0700

    Added a bust checker to inform the player when they bust

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ var blackjack = angular.module("blackjack", []);
 blackjack.controller("blackjackController", ["$scope", "deckBuilder", "totaler", function($scope, deckBuilder, totaler){
   $scope.playerScore = 0;
   $scope.dealerScore = 0;
+  $scope.bust = false;
 
   $scope.deck = deckBuilder.shuffle(deckBuilder.deckBuilder(1));
 
@@ -10,12 +11,18 @@ blackjack.controller("blackjackController", ["$scope", "deckBuilder", "totaler",
   $scope.hiddenCard = [$scope.deck.pop()];
   $scope.playerCards = [$scope.deck.pop(), $scope.deck.pop()];
 
-  $scope.playerTotal = totaler.totalCalculator([$scope.playerCards])[0].join("/");
-  $scope.dealerTotal = totaler.totalCalculator([$scope.dealerCards])[0].join("/");
+  $scope.playerTotalArray = totaler.totalCalculator([$scope.playerCards])[0];
+  $scope.dealerTotalArray = totaler.totalCalculator([$scope.dealerCards])[0];
+  $scope.playerTotal = $scope.playerTotalArray.join("/");
+  $scope.dealerTotal = $scope.dealerTotalArray.join("/");
 
   $scope.hit = function(){
     $scope.playerCards.push($scope.deck.pop());
-    $scope.playerTotal = totaler.totalCalculator([$scope.playerCards])[0].join("/");
+    $scope.playerTotalArray = totaler.totalCalculator([$scope.playerCards])[0];
+    $scope.playerTotal = $scope.playerTotalArray.join("/");
+    if($scope.playerTotalArray[0] > 21){
+      $scope.bust = true;
+    }
   };
   $scope.hold = function(){
     console.log("Hold");
